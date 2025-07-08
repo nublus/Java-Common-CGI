4743ecc28f8cb4c8fe6cc4c3c360fa43a419088d
nublus <1300114067@qq.com>
2025-07-09 01:24:19 +0800

    init

---
+++ src/main/java/com/sln/commoncgi/CommonCgiApplication.java
@@ -0,0 +1,13 @@
+package com.sln.commoncgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCgiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonCgiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sln/commoncgi/CommonCgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sln.commoncgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCgiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
